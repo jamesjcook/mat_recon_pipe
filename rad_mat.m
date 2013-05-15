01da680113d9d8f4c01c05aa8923cedcc707182e
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2013-05-15 19:24:06 +0000

    mat_recon_pipe updated with first functional bruker code, relies on puller and pipeline_utilites
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/mat_recon_pipe/trunk@1289 d41079a3-eaa1-4f09-8506-b324c47726bb

--- rad_mat.m
+++ rad_mat.m
@@ -52,8 +52,10 @@ all_options={'overwrite'
     'write_phase'
     'fp32_magnitude'
     'write_kimage'
+    'write_complex'
     'skip_mem_checks'
-    'testmode'};
+    'testmode'
+    'saveoutput'};
 % make all undefinted options = 0
 for o_num=1:length(all_options)
     if ~exist('opt_struct.(all_options{o})','var')
@@ -83,6 +85,10 @@ for o_num=1:length(options)
             opt_struct.write_kimage=1;
             opt_struct.overwrite=1;
             error('kimages option not fully implemented yet')
+        case 'write_complex'
+            opt_struct.write_kimage=1;
+            opt_struct.overwrite=1;
+            error('kimages option not fully implemented yet')
         case 'i'
             opt_struct.skip_mem_checks=1;
         case ''
@@ -99,6 +105,8 @@ for o_num=1:length(options)
             end
     end
 end
+opt_struct.saveoutput=true;
+statusprints=10;
 data_buffer.scanner_constants=load_scanner_dependency(scanner);
 data_buffer.engine_constants=load_engine_dependency();
 data_buffer.headfile.matlab_functioncall=['rad_mat('''  scanner ''', ''' runno ''', {''' strjoin(input,''', ''') '''} ' ', {''' strjoin(options,''', ''') '''})'];
@@ -126,10 +134,10 @@ data_buffer.input_headfile.origin_path=datapath;
 % display(['base runno is ' runno ' based on given inputs']);
 
 %pull the data to local machine
-local_dir_name= [runno '.work'];
-workdir=[data_buffer.engine_constants.engine_work_directory '/' local_dir_name];
+work_dir_name= [runno '.work'];
+work_dir_path=[data_buffer.engine_constants.engine_work_directory '/' work_dir_name];
 if ~opt_struct.use_existing_data
-    cmd=['puller_simple ' puller_option_string ' ' scanner ' ' puller_data ' ' workdir];
+    cmd=['puller_simple ' puller_option_string ' ' scanner ' ' puller_data ' ' work_dir_path];
     s =system(cmd);
     if s ~= 0
         error('puller failed');
@@ -137,9 +145,11 @@ if ~opt_struct.use_existing_data
 end
 
 % load data header given scanner and directory name
-data_buffer.input_headfile=load_scanner_header(scanner, workdir );
+data_buffer.input_headfile=load_scanner_header(scanner, work_dir_path );
+data_buffer.headfile=combine_struct(data_buffer.headfile,data_buffer.input_headfile);
+data_buffer.headfile=combine_struct(data_buffer.headfile,data_buffer.scanner_constants);
+data_buffer.headfile=combine_struct(data_buffer.headfile,data_buffer.engine_constants);
 clear datapath dirext input puller_data s puller_option_string;
-
 %% determing input acquisition type 
 % some of this might belong in the load data function we're going to need
 data_tag=data_buffer.input_headfile.S_scanner_tag;
@@ -207,7 +217,7 @@ required_free_space=voxel_count*bytes_per_pix_output;
 fprintf('Required disk space is %0.2fMB\n',required_free_space/1024/1024);
 %%% get free space
 
-[status,local_space_bytes] = unix(['df ',data_buffer.engine_constants.engine_work_directory,' | tail -1 | awk ''{print $4}'' ']);
+[~,local_space_bytes] = unix(['df ',data_buffer.engine_constants.engine_work_directory,' | tail -1 | awk ''{print $4}'' ']);
 local_space_bytes=512*str2double(local_space_bytes); %this converts to bytes because default blocksize=512 byte
 %  required_free_space=npoints*ntraces*nblocks*10; %estimate we need at least 10 bytes per image point because we save an unscaled 32 bit and a 16 bit and compelx
 fprintf('Available disk space is %0.2fMB\n',local_space_bytes/1024/1024)
@@ -240,11 +250,11 @@ load_skip            =data_buffer.input_headfile.block_header_size;  %distance b
 ray_blocks           =data_buffer.input_headfile.ray_blocks;         %number of blocks of rays total, sometimes nvolumes, sometimes nslices, somtimes nechoes, ntrs nalphas
 rays_per_block       =data_buffer.input_headfile.rays_per_block;     %number or rays per in a block of input data, 
 ray_length           =data_buffer.input_headfile.ray_length;         %number of samples on a ray, or trajectory (this is doubled due to complex data being taken as real and imaginary discrete samples.)
-ne                   =data_buffer.input_headfile.ne;                 % number of echos.
+% ne                   =data_buffer.input_headfile.ne;                 % number of echos.
 channels             =data_buffer.input_headfile.([data_tag 'channels']); % number of channels.
 
 ray_padding      =0;
-block_factors=factor(ray_blocks);
+% block_factors=factor(ray_blocks);
 
 %%% calculate padding for bruker
 if strcmp(data_buffer.scanner_constants.scanner_vendor,'bruker')
@@ -382,12 +392,12 @@ clear ray_length2 ray_length3 fileInfo bytes_per_vox copies_in_memory in_bitdept
 if ~regexp(runno,'^[A-Z][0-9]{5-6}.*')
     %~strcmp(runno(1),'S') && ~strcmp(runno(1),'N') || length(runno(2:end))~=5 || isnan(str2double(runno(2:end)))
     display('runno does not match CIVM convention, the recon will procede in testmode')
-    testmode=1;
+    opt_struct.testmode=1;
 end
 % if not testmode then create headfile
 if  opt_struct.testmode==1
     display('this recon will not be archiveable');
-    headfile=0;
+%     data_buffer.headfile=0;
 else
     display('gathering gui info');
     display(' ');
@@ -398,7 +408,7 @@ else
 %     end
 %     headfile=create_agilent_headfile(procpar,img_format,runno);
     data_buffer.engine_constants.engine_recongui_menu_path;
-    [s, gui_dump]=system(['$GUI_APP ' ...
+    [~, gui_dump]=system(['$GUI_APP ' ...
         ' ''' data_buffer.engine_constants.engine_constants_path ...
         ' ' data_buffer.engine_constants.engine_recongui_menu_path ...
         ' ' data_buffer.scanner_constants.scanner_tesla ...
@@ -416,17 +426,18 @@ else
     clear gui_info gui_dump gui_info_lines l;
 end
 
-
+%%% this data get for dimensions is temporary, should be handled better in
+%%% the future.
 x=data_buffer.input_headfile.dim_X;
 y=data_buffer.input_headfile.dim_Y;
 z=data_buffer.input_headfile.dim_Z;
-c=data_buffer.input_headfile.([data_tag 'channels'] );
-p=data_buffer.input_headfile.ne;
-t=data_buffer.input_headfile.([data_tag 'volumes'])/c/p;
-input_dimensions=[ x c p z y t] ;
-output_dimensions=[ x y z p c t ];
+channels=data_buffer.input_headfile.([data_tag 'channels'] );
+params=data_buffer.input_headfile.ne;
+timepoints=data_buffer.input_headfile.([data_tag 'volumes'])/channels/params;
+input_dimensions=[ x channels params z y timepoints] ;
+output_dimensions=[ x y z  channels params timepoints ];
 %% do work.
-for c=1:num_chunks
+for chunk_num=1:num_chunks
     %%% LOAD
     chunks_to_load=[1];
     %load data with skips function, does not reshape, leave that to regridd
@@ -452,49 +463,73 @@ for c=1:num_chunks
             fprintf('Data padding retains corrent number of elements, continuing...\n');
         end
     end
+    %%% pre regrid data save.
     kspacedisplaycheck=false;
     if kspacedisplaycheck==true
-        x=data_buffer.input_headfile.dim_X;
-        y=data_buffer.input_headfile.dim_Y;
-        z=data_buffer.input_headfile.dim_Z;
-        channels=data_buffer.input_headfile.([data_tag 'channels'] );
-        p=data_buffer.input_headfile.ne;
-        t=data_buffer.input_headfile.([data_tag 'volumes'])/channels/p;
-        d_reshape=reshape(data_buffer.data,[x channels p z y t]);
-        
-        for pn=1:p
-            for ch=1:channels
-                for zn=1:z
-                    imagesc(log(abs(squeeze(d_reshape(:,ch,1,zn,:,1)))));
-                    pause(4/z/c/p);
-                end
-                fprintf('%d %d %d ',pn,ch,zn);
-            end
-        end
-        
+        input_kspace=reshape(data_buffer.data,input_dimensions);
     end
     %%% REGRID  just simple reshape for cartesian
     rad_regid(data_buffer,c_dims);
+    if kspacedisplaycheck==true
+%         kslice=zeros(size(data_buffer.data,1),size(data_buffer.data,2)*2);
+        kslice=zeros(size(data_buffer.data,1),size(data_buffer.data,2));
+        for tn=1:timepoints
+            for zn=1:z
+                for pn=1:params
+                    for cn=1:channels
+                        fprintf('p:%dc:%d ',pn,cn);
+                        kslice(1:size(data_buffer.data,1),1:size(data_buffer.data,2))=data_buffer.data(:,:,zn,cn,pn,tn);
+%                         kslice(1:size(data_buffer.data,1),size(data_buffer.data,2)+1:size(data_buffer.data,2)*2)=input_kspace(:,cn,pn,zn,:,tn);
+                        imagesc(log(abs(squeeze(kslice))));
+%                             fprintf('.');
+                        pause(4/z/channels/params);
+%                         pause(1);
+%                         imagesc(log(abs(squeeze(input_kspace(:,cn,pn,zn,:,tn)))));
+%                             fprintf('.');
+%                         pause(4/z/channels/params);
+%                         pause(1);
+                    end
+                    fprintf('\n');
+                end
+            end
+        end
+    end
 %     rad_filter(data_buffer,c_dims);
-
-
+    if strcmp(vol_type,'2D')
+        data_buffer.data=reshape(data_buffer.data,[ output_dimensions(1:2) prod(output_dimensions(3:end))] );
+        data_buffer.data=fermi_filter_isodim2(data_buffer.data,'','',true);
+        data_buffer.data=reshape(data_buffer.data,output_dimensions );
+    end
 %% fft
     if strcmp(vol_type,'2D')
         if ~exist('img','var')
             img=zeros(output_dimensions);
         end
-        
-        for cn=1:output_dimensions(5)
-            fprintf('channel %d working...\n',cn);
-            for tn=1:output_dimensions(6)
-                for pn=1:output_dimensions(4)
-                    fprintf('p%d ',pn);
-                    for zn=1:output_dimensions(3)
-                        data_buffer.data(:,:,zn,cn,pn,tn)=fermi_filter_isodim2( data_buffer.data(:,:,zn,cn,pn,tn));
-                        img(:,:,zn,cn,pn,tn)=fftshift(fftshift(ifft2(fftshift(fftshift(data_buffer.data(:,:,zn,cn,pn,tn),1),2)),1),2);
-                        fprintf('.');
+%         xyzcpt
+        for cn=1:channels
+            if statusprints>=10
+                fprintf('channel %d working...\n',cn);
+            end
+            for tn=1:timepoints
+                for pn=1:params
+                    if statusprints>=20
+                        fprintf('p%d ',pn);
                     end
-                    fprintf('\n');
+%                     data_buffer.data(:,:,:,cn,pn,tn)=fermi_filter_isodim2(data_buffer.data(:,:,:,cn,pn,tn),'','',true);
+                    img(:,:,:,cn,pn,tn)=fftshift(ifft2(fftshift(data_buffer.data(:,:,:,cn,pn,tn))));
+
+%                     for zn=1:z
+% %                         data_buffer.data(:,:,zn,pn,cn,tn)=fermi_filter_isodim2(data_buffer.data(:,:,zn,cn,pn,tn));
+% %                         img(:,:,zn,cn,pn,tn)=fftshift(ifft2(data_buffer.data(:,:,zn,cn,pn,tn)));
+%                         img(:,:,zn,cn,pn,tn)=fftshift(ifft2(fftshift(data_buffer.data(:,:,zn,cn,pn,tn))));
+%                         if statusprints>=30
+%                             fprintf('.');
+%                         end
+%                     end
+                    
+                    if statusprints>=20
+                        fprintf('\n');
+                    end
                 end
             end
         end
@@ -505,16 +540,36 @@ for c=1:num_chunks
     
     outputdatacheck=false;
     if outputdatacheck==true
-        for zn=1:output_dimensions(3)
-            for tn=1:output_dimensions(6)
-                for cn=1:output_dimensions(5)
-                    for pn=1:output_dimensions(4)
+        for zn=1:z
+            for tn=1:timepoints
+                for pn=1:params
+                    for cn=1:channels
                         imagesc(log(abs(squeeze(img(:,:,zn,cn,pn,tn)))));
-                        pause(4/z/c/p);
+                        pause(30/z/channels/params);
                         
                     end
                 end
                 fprintf('%d %d\n',zn,tn);
+            end
+        end
+    end
+    combine_channels=false;
+    if combine_channels
+        % image order is expected to be xyzcpt
+        % prod/sum?
+        % 
+        combine_image=zeros([x y z params timepoints]);
+        for tn=1:timepoints
+            for pn=1:params
+                fprintf('p%d',pn);
+                for zn=1:z
+                    for yn=1:y
+                        for xn=1:x
+                            combine_image(xn,yn,zn,pn,tn)=sum(img(xn,yn,zn,:,pn,tn))/sum(img(xn,yn,zn,:,pn,tn));
+                        end
+                    end
+                end
+                fprintf('\n');
             end
         end
     end
@@ -530,18 +585,66 @@ for c=1:num_chunks
 end % end foreachchunk
 
 %% save data
+% this needs a bunch of work, for now it is just assuming the whole pile of
+% data is sitting in memory awaiting saving, does not handle chunks or
+% anything correctly just now. 
 
 %  mag=abs(raw_data(i).data);
 
-for tn=1:output_dimensions(6)
-    for cn=1:output_dimensions(5)
-        for pn=1:output_dimensions(4)
-            make_civm_images();
-            tmp=squeeze(img(:,:,:,cn,pn,tn));
-            outname=[ workdir '/' runno channel_alias(c) num2str((tn-1)*p+p)] ;
-            save_complex(tmp,[ outname '.out']);
-            nii=make_nii(abs(tmp));
-            save_nii(nii,['outname' '.nii']);
+if opt_struct.saveoutput
+    max_mnumber=timepoints*params;
+    m_length=length(num2str(max_mnumber));
+    for tn=1:timepoints
+        for cn=1:channels
+            for pn=1:params
+                tmp=squeeze(img(:,:,:,cn,pn,tn));% pulls out one volume at a time. 
+
+                %%%set channel and mnumber codes in the filename
+                if channels>1
+                    channel_code=channel_alias(cn);
+                else
+                    channel_code='';
+                end
+                m_number=(tn-1)*params+pn;
+                if timepoints> 1 || params >1
+                    m_code=sprintf(['_m%0' num2str(m_length) '.0f'], m_number);
+                else
+                    m_code='';
+                end
+                work_dir_img_path_base=[ work_dir_path '/' runno ] ;
+                work_dir_img_path=[work_dir_img_path_base channel_code m_code];
+                %%% complex save
+                if opt_struct.savecomplex
+                    save_complex(tmp,[ work_dir_img_path '.out']);
+                end
+                %%% nii_save
+                nii=make_nii(abs(tmp), [ data_buffer.headfile.fovx/data_buffer.headfile.dim_X data_buffer.headfile.fovy/data_buffer.headfile.dim_Y data_buffer.headfile.fovz/data_buffer.headfile.dim_Z]); % insert fov settings here ffs....
+                save_nii(nii,[work_dir_img_path '.nii']);
+                if combine_channels
+                    fprintf('Saving combine channel image to output work dir, using rus method\n');
+                    if ~exist([work_dir_img_path_base '.nii'],'file')
+                        nii=make_nii(abs(combine_image), [ ...
+                            data_buffer.headfile.fovx/data_buffer.headfile.dim_X ...
+                            data_buffer.headfile.fovy/data_buffer.headfile.dim_Y ...
+                            data_buffer.headfile.fovz/data_buffer.headfile.dim_Z]); % insert fov settings here ffs....
+                        save_nii(nii,[work_dir_img_path_base '.nii']);
+                    end
+                end
+                %%% civmraw save
+                space_dir_img_name =[ runno channel_code m_code];
+                space_dir_img_folder=[data_buffer.engine_constants.engine_work_directory '/' space_dir_img_name '/' space_dir_img_name 'images' ];
+                if ~exist('outimg_dir_path','file')
+                    mkdir(space_dir_img_folder);
+                end
+                write_headfile([space_dir_img_folder '/' space_dir_img_name '.headfile'],data_buffer.headfile);
+                if (opt_struct.fp32_magnitude==true)
+                    datatype='fp32';
+                else
+                    datatype='raw';
+                end
+                complex_to_civmraw(tmp,[ runno channel_code],data_buffer.scanner_constants.scanner_tesla_image_code,space_dir_img_folder,'auto','',1,datatype)
+                
+            end
         end
     end
 end
--- rad_regid.m
+++ rad_regid.m
@@ -14,32 +14,42 @@ dimensions=...
     [data_buffer.input_headfile.dim_X,...
     data_buffer.input_headfile.([data_buffer.input_headfile.S_scanner_tag 'channels']),...
     data_buffer.input_headfile.ne,... % need to substitute varying parameter.
-    data_buffer.input_headfile.dim_Y,...
     data_buffer.input_headfile.dim_Z,...
-    data_buffer.input_headfile.([data_buffer.input_headfile.S_scanner_tag 'volumes' ])...
+    data_buffer.input_headfile.dim_Y,...
+    data_buffer.input_headfile.([data_buffer.input_headfile.S_scanner_tag 'volumes' ])/data_buffer.input_headfile.([data_buffer.input_headfile.S_scanner_tag 'channels'])/ data_buffer.input_headfile.ne...
     ];
 
 x=data_buffer.input_headfile.dim_X;
 y=data_buffer.input_headfile.dim_Y;
 z=data_buffer.input_headfile.dim_Z;
 c=data_buffer.input_headfile.([data_tag 'channels'] );
-p=data_buffer.input_headfile.ne;
-t=data_buffer.input_headfile.([data_tag 'volumes'])/c/p;
-dimensions=[ x c p z y t] ;
-
+params=data_buffer.input_headfile.ne;
+timepoints=data_buffer.input_headfile.([data_tag 'volumes'])/c/params;
+dimensions=[ x c params z y timepoints] ;
+% [x channels params z y timepoints]
 
 
 % d_reshape=reshape(data_buffer.data,[x channels p z y t]);
 % if cartesian
 
-data_buffer.data=reshape(data_buffer.data,[x c p*z y t] );
-objlist_name=[data_buffer.input_headfile.U_prefix 'ACQ_obj_order'];
-objlist=data_buffer.input_headfile.(objlist_name);
-data_buffer.data(:,:,objlist+1,:)=data_buffer.data;
+%%% use acq key
+acq=true;
+if acq
+    data_buffer.data=reshape(data_buffer.data,[x c params*z y timepoints] );
+    objlist_name=[data_buffer.input_headfile.U_prefix 'ACQ_obj_order'];
+    objlist=data_buffer.input_headfile.(objlist_name);
+    data_buffer.data(:,:,objlist+1,:)=data_buffer.data;
+%%% use pvm key
+else
+    data_buffer.data=reshape(data_buffer.data,[x c params z y timepoints] );
+    objlist_name=[data_buffer.input_headfile.U_prefix 'PVM_ObjOrderList'];
+    objlist=data_buffer.input_headfile.(objlist_name);
+    data_buffer.data(:,:,:,objlist+1,:)=data_buffer.data;
+end
 
 data_buffer.data=reshape(data_buffer.data,dimensions);
 data_buffer.data=permute(data_buffer.data,[1 5 4 2 3 6] );
-
+% [ x y z c p t ]
 
 % maybe we should squeeze....
 % if not cartesian
