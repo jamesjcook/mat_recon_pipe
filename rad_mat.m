faf712d33c3e4a83cbd3777de2ca4da2ac14e309
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2013-10-18 19:45:49 +0000

    mat_recon-pipe, modified the bruker pad code again to run off multiplies of 128 instead of 384, we\'ll see if that helps
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/mat_recon_pipe/trunk@1669 d41079a3-eaa1-4f09-8506-b324c47726bb

--- rad_mat.m
+++ rad_mat.m
@@ -570,17 +570,18 @@ ray_padding      =0;
 
 %%% calculate padding for bruker
 if strcmp(data_buffer.scanner_constants.scanner_vendor,'bruker')
-    if strcmp(data_buffer.input_headfile.([data_prefix 'GS_info_dig_filling']),'Yes')|| ~ignore_errors  %PVM_EncZfRead=1 for fill, or 0 for no fill, generally we fill( THIS IS NOT WELL TESTED)
+    if strcmp(data_buffer.input_headfile.([data_prefix 'GS_info_dig_filling']),'Yes')|| ~opt_struct.ignore_errors  %PVM_EncZfRead=1 for fill, or 0 for no fill, generally we fill( THIS IS NOT WELL TESTED)
         %bruker data is usually padded out to a power of 2 or multiple of
         %3*2^6 940 is
-        % there may be aminimum padding of som enumber?
+        % there may be a minimum padding of some number?
         % have now seen with a 400x2channel acq padding of 96
-        [F,~]=log2(channels*ray_length/(2^6*6));
+        mul=2^6*2;
+        [F,~]=log2(channels*ray_length/(mul));
         %[F,~]=log2(channels*ray_length/(2^5)); % when seeing 96, perhaps
         %this is the clue, padding is a multiple of 2^5th
-        if mod(channels*ray_length,(2^6*6))>0&& F ~= 0.5
+        if mod(channels*ray_length,(mul))>0&& F ~= 0.5
             ray_length2 = 2^ceil(log2(channels*ray_length));
-            ray_length3 = ceil(((channels*(ray_length)))/(2^6*6))*2^6*6;
+            ray_length3 = ceil(((channels*(ray_length)))/(mul))*mul;
             %             [F,~]=log2(ray_length3);
             %             if ray_length3<ray_length2&&F==0.5
             %                 ray_length2=ray_length3;
@@ -612,6 +613,11 @@ if strcmp(data_buffer.scanner_constants.scanner_vendor,'bruker')
     else
         error(['Found no pad option with bruker scan for the first time,' ...
             'Tell james let this continue in test mode']);
+        
+        input_points = 2*ray_length*rays_per_block/channels*ray_blocks;
+        % because ray_length is number of complex points have to doubled this.
+        min_load_size=   ray_length*rays_per_block/channels*(in_bitdepth/8);
+        ray_length=channels*ray_length;
     end
 elseif strcmp(data_buffer.scanner_constants.scanner_vendor,'aspect')
     display('Aspect scan, data size uncertain');
