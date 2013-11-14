dbf65c95a7c96eec99c584be7da4c3da5279a635
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2013-11-14 15:09:07 +0000

    mat_recon_pipe added bruker/luke code for radial3d 2 element coil
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/mat_recon_pipe/trunk@1714 d41079a3-eaa1-4f09-8506-b324c47726bb

---
+++ bruker/luke/Bruker_recon_3Dradial_cryocoil.m
@@ -0,0 +1,53 @@
+function [] = Bruker_recon_3Dradial_cryocoil(runno, dv, kv, dcfv, mat, acq, keynumber)
+%Bruker_recon_3Dradial_cryocoil(runno, dv, kv, dcfv, mat, acq, keynumber)
+% lukes's example code that does one acq's reconstruction. 
+% the whole acq worth of data, co-ords and density compensation must be loaded, 
+% these are dv, kv and dcfv respectively.
+% the save code is just dumped to the end for now.
+%===============INPUTS====================
+%runno=run number
+%dv=k-space data
+%kv=k-space coordinates for the data in dv
+%dcfv=density compensation factors
+%mat=reconstruction matrix size
+%acq=index of acquisition
+%keynumber=keynumber (D'oh!!!!)
+ 
+osf=3; %Oversampling factor
+kw=3; %Kernel width
+RecoScaleChan1=1; %These are variable names used in the Bruker magnet. Use if experimenting with weighted sum of squares recon.
+RecoScaleChan2=1;
+mat2=mat/2;
+recon_mat=mat*osf;
+ 
+kdata(:,:,:,1)=dv(:,1:mat2,:); %Channel 1
+kdata(:,:,:,2)=dv(:,(mat2+1):end,:); %Channel 2
+ 
+tic
+img=zeros([mat mat mat 2],'single');
+% matlabpool local 2    % SAVE FOR LATER, opening matlab pool is slow
+% parfor ind=1:2
+for ind=1:2
+    grid_data=grid3_MAT(kdata(:,:,:,ind), kv, dcfv, recon_mat, 8);  % 0=nonthreaded, 8=threaded
+    grid_datac=complex(grid_data(1:2:end), grid_data(2:2:end));
+    grid_datac=reshape(grid_datac, repmat(recon_mat,[1 3]));
+    % COMMENT THESE LINES FOR PHASE
+%    grid_dataf=hamming_window_3D(recon_mat).*grid_datac; % apply filter
+    grid_dataf=fermi_filter_isodim2(grid_datac);         % test LX filter
+    img_ift=fftshift(abs(ifftn(grid_dataf)));            % ifft
+    img(:,:,:,ind)=crop_center_3D(img_ift, mat);         % crop center
+end
+% matlabpool close
+toc
+ 
+final_recon_im=sqrt((RecoScaleChan1*img(:,:,:,1)).^2+(RecoScaleChan2*img(:,:,:,2)).^2); %Sum of squares (SoS) reconstruction (image domain)
+
+%% Save image
+%image_name=['\B' runno '_acq' num2str(acq) '_key' num2str(keynumber) '.raw'];
+image_name=['B' runno '_acq' num2str(acq) '_key' num2str(keynumber) '.raw'];
+%fid=fopen([current_directory image_name], 'w');
+fid=fopen(image_name, 'w');
+fwrite(fid, final_recon_im, 'float32', 'l'); %Little-endian ordering (lab convention)
+fclose(fid);
+t=toc;
+display(['Reconstructing key ' num2str(keynumber) ' of acquisition ' num2str(acq) ' took ' num2str(t) ' seconds'])
