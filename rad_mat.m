20b2373a8661d16d35c86b78ca057e3a8254726b
James Cook <jamesjcook@users.noreply.github.com>
2014-11-10 16:51:23 -0500

    updated rad_mat to support param files, added rad_mat_bunch to replace radish_scale_bunch, it reads a list file generated in the normal way with listmaker on the scanner. Will try to add list maker support in the future.

--- rad_mat.m
+++ rad_mat.m
@@ -18,6 +18,8 @@ function [img, success_status,data_buffer]=rad_mat(scanner,runno,input_data,opti
 %          aspect id+6000
 %          - for bruker   {'patientid','scanid'} or 'patientid/scanid'
 %                (datanum is not supported yet?)
+%          - list file support? List in radish_scale_bunch format? via
+%          listmaker.
 % option   - for a list and explaination use 'help'.
 %
 % img      - output volume in the output_order
@@ -141,6 +143,7 @@ standard_options={
     'skip_write',             ' do not save anything to disk. good for running inside matlab and continuing in another function'
     'skip_write_civm_raw',    ' do not save civm raw files.'
     'skip_write_headfile',    ' do not write civm headfile output'
+    'skip_write_temp_headfile','Skip writing our temporary headfile before we try to load our data and reconstruct it'
     'write_unscaled',         ' save unscaled nifti''s in the work directory '
     'write_unscaled_nD',      ' save unscaled multi-dimensional nifti in the work directory '
     'display_kspace',         ' display re-gridded kspace data prior to reconstruction, will showcase errors in regrid and load functions'
@@ -168,6 +171,7 @@ beta_options={
     'skip_combine_channels',  ' do not combine the channel images'
     'write_complex',          ' should the complex output be written to th work directory. Will be written as rp(or near rp file) format.'
     'do_aspect_freq_correct', ' perform aspect frequency correction.'
+    'param_file',             ' GUI param file input'
     'pre_defined_headfile',   ' instead of loading the scanner data header load a pre-generated one.'
     'no_scanner_header',      ' the scanner header is invalid un-loadable or misisng for some reason, all required hf keys would need to be specified as options following the unrecognized_ok option. '
     'skip_load'               ' do not load data, implies skip regrid, skip filter, skip recon and skip resort'
@@ -1282,10 +1286,50 @@ if ~regexp(data_buffer.headfile.U_runno,'^[A-Z][0-9]{5-6}.*')
     display('runno does not match CIVM convention, the recon will procede in testmode')
     opt_struct.testmode=1;
 end
+%opt_struct.param_file
 % if not testmode then create headfile
-if  opt_struct.testmode
-    display('this recon will not be archiveable, rerun same command with skip_recon to rewrite just the headfile using the gui settings.');
+if islogical(opt_struct.param_file) 
+    if opt_struct.paramfile
+        warn('You tried to specifiy a param file, but forgot to enter it.');
+        pause( opt_struct.warning_pause ) ;
+    end
+    if opt_struct.debug_mode>=10 
+        display('Gathering gui info ...');
+    end
     data_buffer.engine_constants.engine_recongui_menu_path;
+    [~, gui_dump]=system(['$GUI_APP ' ...
+        ' ''' data_buffer.engine_constants.engine_constants_path ...
+        ' ' data_buffer.engine_constants.engine_recongui_menu_path ...
+        ' ' data_buffer.scanner_constants.scanner_tesla ...
+        ' ''']);
+    use_GUI_DUMP=true;
+    %opt_struct.param_file
+elseif ~islogical(opt_struct.param_file) 
+    if ~exist([ data_buffer.engine_constants.engine_recongui_paramfile_directory '/' opt_struct.param_file ],'file')
+        %%% this should also support regen of param file....
+        [~, param_file_name, e]=fileparts(opt_struct.param_file);
+        param_file_name=[param_file_name e ];
+        [~]=system(sprintf('%s '' %s %s %s'' ',getenv('GUI_APP') ,...
+            data_buffer.engine_constants.engine_constants_path ,...%    ec.engine_recongui_menu_path ,...
+            scanner ,... %     ' ' sc.scanner_tesla ...
+            param_file_name ...
+            ));
+    end
+    if ~exist([ data_buffer.engine_constants.engine_recongui_paramfile_directory '/' opt_struct.param_file ],'file')
+        error('Param file %s/%s failed to generate!',data_buffer.engine_constants.engine_recongui_paramfile_directory,opt_struct.param_file );
+    end
+    use_GUI_DUMP=false;
+    pf=read_headfile([ data_buffer.engine_constants.engine_recongui_paramfile_directory '/' opt_struct.param_file ]);
+    data_buffer.input_headfile=combine_struct(data_buffer.input_headfile,pf,'U_');
+    data_buffer.headfile      =combine_struct(data_buffer.headfile,pf,'U_');
+% pf.fields=fieldnames(pf);
+% gui_info_lines=cell(numel(pf.fields),1);
+% for fn=1:numel(pf.fields)
+%     gui_info_lines{fn}=[pf.fields(fn) ':::' pf.(pf.fields{fn}) ];
+% end
+elseif opt_struct.testmode
+    display('this recon will not be archiveable, rerun same command with skip_recon to rewrite just the headfile using the gui settings.');
+%     data_buffer.engine_constants.engine_recongui_menu_path;
     [~, gui_dump]=system(['$GUI_APP ' ...
         ' ''' data_buffer.engine_constants.engine_constants_path ...
         ' ' data_buffer.engine_constants.engine_recongui_menu_path ...
@@ -1294,26 +1338,23 @@ if  opt_struct.testmode
         ' ''']);
     gui_info_lines=strtrim(strsplit(gui_dump,' '));
     gui_dump=strjoin(gui_info_lines,':::test\n');
-else
-    display('gathering gui info');
-    display(' ');
-    data_buffer.engine_constants.engine_recongui_menu_path;
-    [~, gui_dump]=system(['$GUI_APP ' ...
-        ' ''' data_buffer.engine_constants.engine_constants_path ...
-        ' ' data_buffer.engine_constants.engine_recongui_menu_path ...
-        ' ' data_buffer.scanner_constants.scanner_tesla ...
-        ' ''']);
+    use_GUI_DUMP=true;
 end
 
-gui_info_lines=strtrim(strsplit(gui_dump,'\n'));
-for l=1:length(gui_info_lines)
-    gui_info=strsplit(gui_info_lines{l},':::');
-    if length(gui_info)==2
-        data_buffer.headfile.(['U_' gui_info{1}])=gui_info{2};
-        data_buffer.input_headfile.(['U_' gui_info{1}])=gui_info{2};
-        fprintf('adding meta line %s=%s\n', ['U_' gui_info{1}],data_buffer.headfile.(['U_' gui_info{1}]));
-    else
-        fprintf('ignoring gui input line:%s\n',gui_info_lines{l});
+if use_GUI_DUMP
+    if isempty(gui_info_lines) && ~opt_struct.ignore_errors
+        error('GUI did not return values!');
+    end
+    gui_info_lines=strtrim(strsplit(gui_dump,'\n'));
+    for l=1:length(gui_info_lines)
+        gui_info=strsplit(gui_info_lines{l},':::');
+        if length(gui_info)==2
+            data_buffer.headfile.(['U_' gui_info{1}])=gui_info{2};
+            data_buffer.input_headfile.(['U_' gui_info{1}])=gui_info{2};
+            fprintf('adding meta line %s=%s\n', ['U_' gui_info{1}],data_buffer.headfile.(['U_' gui_info{1}]));
+        else
+            fprintf('ignoring gui input line:%s\n',gui_info_lines{l});
+        end
     end
 end
 
@@ -1323,10 +1364,7 @@ if isfield(data_buffer.headfile,'U_specid')
         opt_struct.skip_combine_channels=true;
     end
 end
-if isempty(gui_info_lines) && ~opt_struct.ignore_errors
-    error('GUI did not return values!');
-end
-clear gui_info gui_dump gui_info_lines l;
+clear gui_info gui_dump gui_info_lines l pf use_GUI_DUMP;
 %% fancy dimension settings before reconstruction
 %%% this data get for dimensions is temporary, should be handled better in
 %%% the future.
@@ -1384,13 +1422,13 @@ fprintf(['recon proceding of file at %s\n'...
     data_in.precision_string,...
     data_in.disk_endian);
 
-% if ~opt_struct.skip_write_headfile
+ if ~opt_struct.skip_write_temp_headfile
     dest=[data_buffer.headfile.work_dir_path '/' 'rad_mat.headfile'];
     fprintf('\ttemp headfile save \n\t\t%s\n',dest);
 %     data_buffer.headfile.output_image_path=space_dir_img_folder;
     write_headfile(dest,data_buffer.headfile,0);
     % insert validate_header perl script check here?
-% end
+ end
 
 clear dim_order ds dest ;
 
@@ -2944,6 +2982,8 @@ if ~opt_struct.skip_recon||opt_struct.force_ij_prompt
     [~,txt]=system('echo -n $ijstart');  %-n for no newline i think there is a smarter way to get system variables but this works for now.
     ij_prompt=sprintf('%s -macro %s',txt, openmacro_path);
     mat_ij_prompt=sprintf('system(''%s'');',ij_prompt);
+    data_buffer.headfile.rad_mat_ij_macro=openmacro_path;
+    data_buffer.headfile.rad_mat_ij_macro_prompt=ij_prompt;
 end
 if ~isempty(ij_prompt)&& ~opt_struct.skip_write_civm_raw
     fprintf('test civm image output from a terminal using following command\n');
@@ -3032,7 +3072,7 @@ if ~opt_struct.skip_write_civm_raw && ~opt_struct.skip_recon && isfield(data_buf
             %     xroll=round(centroid(1)-d_struct.x/2);
             %     yroll=d_struct.y-round(centroid(2)-d_struct.y/2);
             %     zroll=round(centroid(3)-d_struct.z/2);
-            cmd_list{c_r}=sprintf('roll_3d -x %d -y %d -z %d %s;',roll.x(c_r),roll.y(c_r), roll.z(c_r), run_string);
+            cmd_list{c_r}=sprintf('roll_3d -x %d -y %d -z %d %s',roll.x(c_r),roll.y(c_r), roll.z(c_r), run_string);
         end
         fprintf('Use terminal to run roll_3d with command, (Replace the number''s with your deisred roll, example values are a best guess based on a find minimum calculation.)\n%s\n\n', strjoin(cmd_list,'\n'));
         fprintf('OR run roll_3d in matlab using \n');
@@ -3042,6 +3082,7 @@ if ~opt_struct.skip_write_civm_raw && ~opt_struct.skip_recon && isfield(data_buf
             roll_prompt=sprintf('%ssystem(''%s'');\n',roll_prompt,cmd_list{cmd_n});
         end
         fprintf('%s',roll_prompt);
+        data_buffer.headfile.rad_mat_roll_prompt=strjoin(cmd_list,'\n');
     end
     
 end
---
+++ rad_mat_bunch.m
@@ -0,0 +1,132 @@
+function imglist=rad_mat_bunch(scanner,input,rad_options)
+% RAD_MAT_BUNCH(scanner,input)
+% input is either 
+%    'Name.list', the list file on the scanner(like radish_scale_bunch).
+%  OR the parameters to list maker.
+%    'runno study series count'
+%    'runno study series count first_num
+
+% look at radish_scale_bunch
+% see what work needs to translate into rad_mat.
+% should we support load multiple files or should we just wrap rad_mat with a rad_mat_bunch.
+
+ts=tic;
+%%%% To properly drop radish_scale_bunch relying on radish.
+% add param_file support to rad_mat
+% add the radish gui error checking to rad_mat
+% 
+% modify radish scale bunch to call rad_mat from the command line.
+% 
+% fix rad_mat to be callable from the command line. 
+% example list format
+%studyname/series##.fid;outrunno, studyname/series##.fid;outrunno
+
+%% list load
+%%% if not list input
+%         "runnumber:$runno\n".
+% 		  "study:    $study\n".
+% 		  "series:   $series\n".
+% 		  "scans:    $count\n".
+% 		  "first_num:$m_start\n");
+
+sc=load_scanner_dependency(scanner);
+ec=load_engine_dependency;
+% =ec.scanner_data_directory;
+
+if ( regexpi(input,'^[a-zA-Z][0-9]{5,6}\w*[.]list$'))
+    list_file=input;
+    input=strsplit(input,'.');
+    base_runno=input{1}; 
+else % make list
+    %     specid_s=strsplit(data_buffer.input_headfile.U_specid,';');
+    input=strsplit(input,' ');
+    base_runno=input{1};
+%     scanner_study=input{2};
+%     scanner_series=input{3};
+%     nscanns=input{4};
+%     if numel(input)>4
+%         starting_num=input{5};
+%     else
+%         starting_num='';
+%     end
+    list_file=[base_runno '.list'];
+%     error('generating list on fly not supported yet');
+% cmd=sprintf('ssh sc.scanner_user@system ~/bin/listmaker %s,input);
+% system(cmd);
+end
+% ssh omega@kammy \$HOME/bin/listmaker
+% get list.
+%usage: puller_simple  device file/folder local_dest_dir  result_file_basename(blank for no change)
+cmd=sprintf('puller_simple -of file %s %s %s.work',scanner,list_file,base_runno);
+disp(cmd);
+system(cmd);
+
+
+%% load list
+list='S65460_01/ser02.fid;S65460_m01, S65460_01/ser03.fid;S65460_m02, S65460_01/ser04.fid;S65460_m03, S65460_01/ser05.fid;S65460_m04, S65460_01/ser06.fid;S65460_m05, S65460_01/ser07.fid;S65460_m06, S65460_01/ser08.fid;S65460_m07';
+list=strsplit(list,', '); 
+%S65460.list
+opts={'debug_mode=0','warning_pause=0','skip_fft=0','skip_write_temp_headfile','write_complex'};
+if exist('rad_options','var')
+opts=[opts,rad_options];
+end
+%% param gen % integrated!
+%%%     eval $GUI_APP \'`ls ${WKS_SETTINGS}/engine_deps/engine_${RECON_HOSTNAME}_radish_dependencies` $@ \'
+%%% scanner new filename.
+% [~]=system(sprintf('%s '' %s %s %s.param'' ',getenv('GUI_APP') ,...
+%     ec.engine_constants_path ,...%    ec.engine_recongui_menu_path ,...
+%     scanner ,... %     ' ' sc.scanner_tesla ...
+%     base_runno ...
+%     ));
+%     
+%% run recons
+runno_text='';
+runno_list=cell(1,numel(list));
+runno_openmacro_paths=cell(1,numel(list));
+runno_roll_prompts=cell(1,numel(list));
+for i=1:numel(list)
+    le=strsplit(list{i},';');
+    data=le{1};
+    runno=le{2};
+    runno_list{i}=runno;
+    %foreach piece of data
+    [~,~,imglist.(['img' num2str(i)])]=rad_mat(scanner,runno,data,[opts,sprintf('param_file=%s.param',base_runno)]);
+    runno_text=sprintf('%s %s',runno_text,runno);
+    runno_openmacro_paths{i}=imglist.(['img' num2str(i)]).headfile.rad_mat_ij_macro;
+    runno_roll_prompts{i}=imglist.(['img' num2str(i)]).headfile.rad_mat_roll_prompt;
+end
+imglist.runo_text=runno_text;
+
+%% combine output into single image, scaleit save it.
+cmd=sprintf('reform_group %s', runno_text);
+system(cmd);
+toc(ts)
+
+%% convenience prompts
+%%% view
+%  -eval "macro code"
+% -run "runMacro("path");
+[~,txt]=system('echo -n $ijstart');  %-n for no newline i think there is a smarter way to get system variables but this works for now.
+% ij_prompt=sprintf('%s -macro %s',txt,strjoin(runno_openmacro_paths, ' -macro '));
+% ij_prompt=sprintf('%s -run ''runMacro("%s");''',txt,strjoin(runno_openmacro_paths,'");runMacro("'));
+ij_prompt=sprintf('%s -eval ''run("CIVM RunnoOpener","headfile=%s/%s.headfile loadallheadfiles volume_combine_threshold=1");''',txt,imglist.img1.headfile.output_image_path,imglist.img1.headfile.U_runno);
+mat_ij_prompt=sprintf('system(''%s'');',ij_prompt);
+% /panoramaspace/S65460_m01/S65460_m01images/S65460_m01.headfile
+fprintf('test civm image output from a terminal using following command\n');
+fprintf('  (it may only open the first and last in large sequences).\n');
+fprintf('\n\n%s\n\n\n',ij_prompt);
+fprintf('test civm image output from matlab using following command\n');
+fprintf('  (it may only open the first and last in large sequences).\n');
+fprintf('\n%s\n\n',mat_ij_prompt);
+imglist.ij_prompt=ij_prompt;
+
+%%%archivetag
+archive_tag_output=sprintf('archiveme %s %s',imglist.img1.headfile.U_civmid,strjoin(runno_list, ' '));
+fprintf('initiate archive from a terminal using following command, (should change person to yourself). \n\n\t%s\n\n OR run archiveme in matlab useing \n\tsystem(''%s'');\n',archive_tag_output,archive_tag_output);
+imglist.archive_tag_output=archive_tag_output;
+%%% roll prompt
+%         data_buffer.headfile.rad_mat_roll_prompt=roll_prompt;
+% roll_3d -x %d -y %d -z %d %s;
+runno_list(1)=[];
+fprintf('%s %s\n',strtrim(runno_roll_prompts{1}),strjoin(runno_list,' '));
+imglist.runno_roll_prompts=runno_roll_prompts;
