5eb50e81537f5fea7c835587ac4a30b6c12710b3
James Cook <jamesjcook@users.noreply.github.com>
2015-07-01 12:01:01 -0400

    Added recon_strategy testing files

--- get_recon_strategy_test.m
+++ get_recon_strategy_test.m
@@ -1,10 +1,21 @@
-test_case.lme=true;
-test_case.n3d=true;
-if test_case.lme
-load('get_recon_strategy_test');
-[recon_strategy_lme,opt_struct]=get_recon_strategy3(data_buffer,opt_struct,d_struct,data_in,data_work,data_out,meminfo);
+function [recon_strategy,opt_struct]=get_recon_strategy_test(testcase)
+
+if ~exist('testcase','var')
+    error(' Did not specify test case, choose one of l_me, s3d, l_m3d');
 end
-if test_case.n3d
-load('get_recon_strategy_test_single3d')
-[recon_strategy,opt_struct]=get_recon_strategy3(data_buffer,opt_struct,d_struct,data_in,data_work,data_out,meminfo);
+
+test_case.(testcase)=1;
+if isfield(test_case,'l_me')
+    load('get_recon_strategy_test_large_multi_echo');
+    [recon_strategy,opt_struct]=get_recon_strategy3(data_buffer,opt_struct,d_struct,data_in,data_work,data_out,meminfo);
 end
+if isfield(test_case,'s3d')
+    % 512,256,256 single 3d
+    load('get_recon_strategy_test_single3d')
+    [recon_strategy,opt_struct]=get_recon_strategy3(data_buffer,opt_struct,d_struct,data_in,data_work,data_out,meminfo);
+end
+if isfield(test_case,'l_m3d')
+    load('get_recon_strategy_test_many3D_large')
+    [recon_strategy,opt_struct]=get_recon_strategy3(data_buffer,opt_struct,d_struct,data_in,data_work,data_out,meminfo);
+end
+
