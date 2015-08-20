f16af49f7ece4350b535fb093e4e4a1463c618e1
James Cook <jamesjcook@users.noreply.github.com>
2015-08-20 14:03:34 -0400

    Added more testing info, new code for new system installation with different username.

--- examples/bruker_tests.m
+++ examples/bruker_tests.m
@@ -6,24 +6,28 @@ all_methods={'AdjRefG','DtiEpi','DtiStandard','EPI','FC2D_ANGIO','FLASH',...
     'RAREVTR','SINGLEPULSE','T1_EPI','UTE','UTE3D','ute3d_keyhole',''};
 fprintf('All Methods\n ');
 fprintf('%s,\n ',all_methods{:});
-known_methods={'MDEFT','MGE','MSME','RARE','UTE3D','ute3d_keyhole'};
+% known_methods={'MDEFT','MGE','MSME','RARE','UTE3D','ute3d_keyhole'};
 % known_methods={'MGE'};
-% known_methods={'ute3d_keyhole','UTE3D'};
+known_methods={'ute3d_keyhole','UTE3D'};
 
 fprintf('Known/tested Methods\n ');
 fprintf('%s,\n ',known_methods{:});
-dataroot='/opt/PV5.1/data/nmrsu/nmr/';
 verbosity=0;
 scanner_list={'dory','nemo'};
 scanner_list={'nemo'};
+scanner_list={'nemoPV6'};
+b_user='mri';
+dataroot=['/opt/PV5.1/data/' b_user '/nmr/'];
+dataroot=['/opt/PV6/data/' b_user '/'];
 fail_list='';
-test_brukerextract=false;
+test_brukerextract=true;
 test_rad_mat=true;
 
 for scanner_num=1:numel(scanner_list)
+    sc=load_engine_dependency(scanner);
     scanner=scanner_list{scanner_num};
     % to get all methods currently avaialable on scanner
-    [s, method_list]=system([ 'ssh nmrsu@' scanner ' grep Method= ' dataroot '*/*/method | cut -d "=" -f2 |sort -u']);
+    [s, method_list]=system([ 'ssh ' b_user '@' scanner ' grep Method= ' dataroot '*/*/method | cut -d "=" -f2 |sort -u']);
     method_list(end)=[];
     method_list=strsplit(method_list,'\n');
     fprintf('Available Test Methods\n ');
@@ -34,7 +38,7 @@ for scanner_num=1:numel(scanner_list)
         recon_status=false;
         if regexp(method,strjoin(known_methods,'|'))
             fprintf('Procesing scans for method: %s\n',method);
-            [s, acq_list]=system( ['ssh nmrsu@' scanner ' grep Method= ' dataroot '*/*/method | grep ' method '|cut -d ":" -f 1' ]);
+            [s, acq_list]=system( ['ssh ' b_user '@' scanner ' grep Method= ' dataroot '*/*/method | grep ' method '|cut -d ":" -f 1' ]);
             acq_list=strsplit(acq_list,'\n');
             acq_list(end)=[];
             % patient_id='20131025';
@@ -48,7 +52,7 @@ for scanner_num=1:numel(scanner_list)
                 details=strsplit(acq,'/');
                 patient_dir=details{end-2};
                 if ~strcmp(patient_dir,last_patient_dir)
-                    [s, study_val] = system([ 'ssh nmrsu@' scanner ' grep -HiC 1 study_name= ' dataroot '/' patient_dir '/subject | tail -n 1 | cut -d "<" -f2']);
+                    [s, study_val] = system([ 'ssh ' b_user '@' scanner ' grep -HiC 1 study_name= ' dataroot '/' patient_dir '/subject | tail -n 1 | cut -d "<" -f2']);
                     study_fieldname=['sid_' study_val(1:end-2)];
                     if isempty(study_fieldname) ||isempty(study_val) % check for spaces.
                         error('studyblank');
@@ -117,30 +121,37 @@ for scanner_num=1:numel(scanner_list)
                     studyname=studyname(5:end);
                     acquisition_numbers=patients.(pname).(sname);
                     fprintf('Found %d scans in study %s\n',numel(acquisition_numbers),studyname);
-                    for acq_num=1:1 %numel(acquisition_numbers) %only do first acq to collect traj files
+                    for acq_num=1:numel(acquisition_numbers) %only do first acq to collect traj files
                         num=acquisition_numbers(acq_num);
                         if ~recon_status
+                            if strcmp(b_user,'mri')
+                                stmp=scanner;
+                                scanner=[scanner b_user];
+                            end
+                            rad_cmd=[ 'rad_mat(''' scanner ''',' [ '''test_RM_' scanner '_' method '_' patient_id '_' studyname '_s' sprintf('%02d',num)] ''',{''' patient_id ''',''' num2str(num) '''});'];
+                            fprintf('%s\n',rad_cmd);
                             if test_rad_mat
                                 try
                                     [img,recon_status ]=rad_mat(scanner,...
                                         ['test_RM_' scanner '_' method '_' patient_id '_' studyname '_s' sprintf('%02d',num)],...
                                         {patient_id,num2str(num)},...
                                         {'warning_pause=0'
-                                        'debug_mode=0'
-                                        ['study=' studyname]
+                                        'debug_mode=50'
                                         'overwrite'
                                         'existing_data'
                                         'testmode'
                                         'write_unscaled_nD'
-                                        'skip_write_civm_raw'
-                                        'skip_write_headfile'
                                         'ignore_errors'
-                                        'skip_fft'
-                                        'skip_regrid'
-                                        'skip_filter'
-                                        'skip_write'
                                         }');
-                                    recon_status=false;
+%                                     
+%                                         ['study=' studyname]
+%                                         'skip_write_civm_raw'
+%                                         'skip_write_headfile'
+%                                         'skip_fft'
+%                                         'skip_regrid'
+%                                         'skip_filter'
+%                                         'skip_write'
+%                                     recon_status=false;
                                     
                                     %         if s==0
                                     %         fail_list=sprintf('%s\n%s',fail_list,num2str(num));
@@ -158,7 +169,11 @@ for scanner_num=1:numel(scanner_list)
                                 end
                             end
                             if test_brukerextract
-                                system(['brukerextract',' ','-p',' ','test.param',' ',scanner,' ','nmrsu',' ',patient_id,' ',studyname,' ',num2str(num),' ',['test_BE_' scanner '_' method '_' patient_id '_' studyname '_s' sprintf('%02d',num) ]],'-echo');
+                                system(['brukerextract',' ','-p',' ','test.param',' ',scanner,' ', b_user ,' ',patient_id,' ',studyname,' ',num2str(num),' ',['test_BE_' scanner '_' method '_' patient_id '_' studyname '_s' sprintf('%02d',num) ]],'-echo');
+                            end
+                            if exist('stmp','var')
+                                scanner=stmp;
+                                clear stmp;
                             end
                         end
                     end
