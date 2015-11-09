d6158c565b3670c119b562515dff269480abe8b2
James Cook <jamesjcook@users.noreply.github.com>
2015-11-09 11:12:47 -0500

    Updated sliding window example

--- grid/DCE_ExampleRecon.m
+++ grid/DCE_ExampleRecon.m
@@ -15,18 +15,47 @@ opt_struct.good='true';
 % nchannels=4
 %%B02124
 opt_struct.dataFile = '/Users/james/Desktop/data/B02124/fid';
-opt_struct.window_width=13;
+
 opt_struct.trajectoryOnly=0;
 % if exist('B02124','var')
     B02124=large_array;
     
 % end
-i=opt_struct.window_width;
-sliding_window_recon(B02124,opt_struct)
-if ~isfield(opt_struct,'trajectoryOnly')|| ( isfield(opt_struct,'trajectoryOnly') && opt_struct.trajectoryOnly==0)
-    save_nii(make_nii(B02124.data),sprintf('/piperspace/B02124.work/B02124_win%d.nii',i));
+win_val=[13,7,3,1];
+for n=1:length(win_val)
+    B02124=large_array;
+    opt_struct.window_width=win_val(n);
+    opt_struct.sharpness=0.21;
+    sliding_window_recon(B02124,opt_struct)
+    if ~isfield(opt_struct,'trajectoryOnly')|| ( isfield(opt_struct,'trajectoryOnly') && opt_struct.trajectoryOnly==0)
+        save_nii(make_nii(B02124.data),sprintf('/piperspace/B02124.work/B02124_win%d_shrp%g.nii',opt_struct.window_width,opt_struct.sharpness));
+    end
 end
 
+win_val=[7,3,1];
+sharp_vals=[0.25,0.3,0.35];
+for n=1:length(win_val)
+    B02124=large_array;
+    opt_struct.window_width=win_val(n);
+    opt_struct.sharpness=sharp_vals(n);
+    sliding_window_recon(B02124,opt_struct)
+    if ~isfield(opt_struct,'trajectoryOnly')|| ( isfield(opt_struct,'trajectoryOnly') && opt_struct.trajectoryOnly==0)
+        save_nii(make_nii(B02124.data),sprintf('/piperspace/B02124.work/B02124_win%d_shrp%g.nii',opt_struct.window_width,opt_struct.sharpness));
+    end
+end
+
+sharp_vals=[0.2,0.3,0.4,0.5,0.6,0.7];
+for n=1:length(sharp_vals)
+    B02124=large_array;
+    opt_struct.window_width=3;
+    opt_struct.sharpness=sharp_vals(n);
+    sliding_window_recon(B02124,opt_struct)
+    if ~isfield(opt_struct,'trajectoryOnly')|| ( isfield(opt_struct,'trajectoryOnly') && opt_struct.trajectoryOnly==0)
+        save_nii(make_nii(B02124.data),sprintf('/piperspace/B02124.work/B02124_win%d_shrp%g.nii',opt_struct.window_width,opt_struct.sharpness));
+    end
+end
+
+if exist('continue','var')
 %%B02126
 opt_struct.dataFile ='/Users/james/Desktop/data/B02126/fid';
 % if ~exist('B02126','var')
@@ -35,4 +64,5 @@ opt_struct.dataFile ='/Users/james/Desktop/data/B02126/fid';
 sliding_window_recon(B02126,opt_struct)
 if ~isfield(opt_struct,'trajectoryOnly')|| ( isfield(opt_struct,'trajectoryOnly') && opt_struct.trajectoryOnly==0)
 save_nii(make_nii(B02126.data),sprintf('/piperspace/B02126.work/B02126_win%d.nii',i));
+end
 end
