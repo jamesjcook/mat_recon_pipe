8049e591e10ea2ad87f05de7a15a7cd6edd27b02
James Cook <jamesjcook@users.noreply.github.com>
2016-01-21 17:26:47 -0500

    Updated scott_grid stub to set necessary variables.

--- grid/scott_grid.m
+++ grid/scott_grid.m
@@ -1,6 +1,14 @@
 function scott_grid(data_buffer,opt_struct,data_in,data_work,data_out)
 % go and call on scott's  functionized example grid.
-
+if ~exist('data_buffer','var')
+    load /Volumes/workstation_home/software/recon/External/ScottHaileRobertson/Duke-CIVM-MRI-Tools/DukePackage/Recon/ReconScripts/Bruker/ScottDemoVarLoad_B02124;
+end
+if ~isfield(opt_struct,'nThreads')
+    opt_struct.nThreads = feature('numCores');
+end
+if ~isfield(opt_struct,'window_width')
+    opt_struct.window_width=13;
+end
 % Dirty find and setup function should fail if we dont have them.
 % run([getenv('WORKSTATION_HOME') '/recon/External/ScottHaileRobertson/CIVM_MATLAB_Libs/setup.m']);
 run([getenv('WORKSTATION_HOME') '/recon/External/ScottHaileRobertson/Duke-CIVM-MRI-Tools/setup.m']);
