037676ae37f023f6ade1c93633493aae89cbaf2e
James Cook <jamesjcook@users.noreply.github.com>
2016-02-19 09:16:26 -0500

    Added Z encoding support. Added option to recon partial kspace volumes too with do_recon. Documentation of do_recon not exposed to users.

--- rad_mat_chunk.m
+++ rad_mat_chunk.m
@@ -30,10 +30,20 @@ else
     testing_options={};
     patient_id=input{1};
     scan_ids=input{2};
-    partial_options={'skip_recon','testmode'};
+    partial_options={'testmode'};
 end
 if ~exist('options','var')
     options={};
+end
+if ~exist('partial_options','var')
+    partial_options={};
+end
+
+
+% ind=find(ismember(options,'do_recon'));
+ind=find(strcmp('do_recon',options)); % inthory much faster.
+if isempty(ind)
+    partial_options=[partial_options,{'skip_recon'}];
 end
 if ~strcmp(scanner,'nemo')...
         && ~strcmp(scanner,'centospc')
@@ -64,12 +74,13 @@ pda=cell(1,length(scan_ids));
 sn=1;
 pull_dir{sn}=sprintf('/%s/%s_%i.work/',data_dir,runno,scan_ids(sn));
 [~,~,pda{sn}]=rad_mat(scanner,sprintf('%s_%i',runno,scan_ids(sn)),sprintf ('%s/%i',patient_id,scan_ids(sn)) ,...
-    [ testing_options,'skip_recon',...
+    [ testing_options,partial_options,...
     rad_mat_options,...
     required_options_for_unknown_sequence,...
     options]);
 data_files{sn}=sprintf('%s/fid',pull_dir{sn});
 % initally was parfor because it too SOOOO long, Unfortunately we crashed(or brought to crawl) the lascola magnet.
+cumulative_phase_2{sn}=pda{sn}.input_headfile.z_Bruker_ACQ_spatial_phase_2;
 for sn=2:length(scan_ids)
     pull_dir{sn}=sprintf('/%s/%s_%i.work/',data_dir,runno,scan_ids(sn));
     [~,~,pda{sn}]=rad_mat(scanner,sprintf('%s_%i',runno,scan_ids(sn)),sprintf ('%s/%i',patient_id,scan_ids(sn)) ,...
@@ -78,18 +89,21 @@ for sn=2:length(scan_ids)
         required_options_for_unknown_sequence,...
         options]);
     data_files{sn}=sprintf('%s/fid',pull_dir{sn});
+   cumulative_phase_2{sn}=pda{sn}.input_headfile.z_Bruker_ACQ_spatial_phase_2;
 end
 runno_workdir= sprintf('/%s/%s.work',data_dir,runno);
-if ~exist([ runno_workdir '/fid' ])
+
+pda{end}.input_headfile.kspace_data_path=sprintf('%s/fid',runno_workdir); % update the dataa path for our final input header.
+if ~exist(pda{end}.input_headfile.kspace_data_path,'file')
   system(sprintf('mkdir -p %s',runno_workdir));
-  cmd=sprintf('cp -Ppf %s/* %s',pull_dir{end},runno_workdir);
-  fprintf('%s\n\n',cmd);
-  system(cmd);
-
-  cmd=sprintf('rm %s/fid',runno_workdir);
-  fprintf('%s\n\n',cmd);
-  system(cmd);
-
+  if exist('copy_entier_final_scan','var')
+      cmd=sprintf('cp -Ppf %s/* %s',pull_dir{end},runno_workdir);
+      fprintf('%s\n\n',cmd);
+      system(cmd);
+      cmd=sprintf('rm %s/fid',runno_workdir);
+      fprintf('%s\n\n',cmd);
+      system(cmd);
+  end
   %% concatenate data to new file
   cmd=sprintf('cat %s > %s/fid',strjoin(data_files,' '),runno_workdir);
   fprintf('%s\n\n',cmd);
@@ -98,11 +112,31 @@ if ~exist([ runno_workdir '/fid' ])
   %fh=read_headfile(sprintf('%s/bruker.headfile',runno_workdir));
   % fh=pda.headfile;
 end
-delete(gcp('nocreate'));
+%% create chunk_headfile and save it to working directory, set options to load that instead of using bruker crafted headfile. 
+%%%% USE INPUT_HEADFILE FIELD FOR FRADMAT? CIRCUMVENT DUMPHEADER TO CONJOIN
+%%%% THE PHASE TWO INFORMATION
+
+%update spatial_phase_2 to contain all the elements of the spatial phase
+pda{end}.input_headfile.z_Bruker_ACQ_spatial_phase_2=cell2mat(cumulative_phase_2);
+%create new encoding steps based ont he sorted indicies of the cumulative
+%spatial phase. 
+[~, pda{end}.input_headfile.z_Bruker_EncSteps2]=sort(pda{end}.input_headfile.z_Bruker_ACQ_spatial_phase_2);
+% set z_encode, normally handled by dumpHeader perl script
+pda{end}.input_headfile.dim_Z_encoding_order=pda{end}.input_headfile.z_Bruker_EncSteps2;
+pda{end}.input_headfile.ray_blocks=pda{end}.input_headfile.dim_Z*numel(scan_ids);% update ray_blocks
+pda{end}.input_headfile.dim_Z=pda{end}.input_headfile.dim_Z*numel(scan_ids);%update dim_Z
+pda{end}.input_headfile.fovz=pda{end}.input_headfile.fovz*numel(scan_ids);% update fovz
+chunk_headfile=sprintf('%s/CONCAT.headfile',runno_workdir); %set chunk_headfile path
+write_headfile(chunk_headfile,pda{end}.input_headfile);
+
+delete(gcp('nocreate')); %shuts down parpool if we're using it.
 %% customized rad_mat call
 % 
-required_options_to_multi_part=[required_options_to_multi_part,{sprintf('ray_blocks=%i', pda{end}.headfile.dim_Z*numel(scan_ids)),sprintf('dim_Z=%i',pda{end}.headfile.dim_Z*numel(scan_ids)),sprintf('fovz=%i', pda{end}.headfile.fovz*numel(scan_ids))}];
-[success_status,img, buffer]=rad_mat(scanner,runno,['rad_mat_chunk',patient_id,strjoin(strsplit(num2str(scan_ids),' '),'_')],[ {'existing_data','overwrite'},...
+required_options_to_multi_part=[required_options_to_multi_part,...
+    {'no_scanner_header',sprintf('pre_defined_headfile=%s',chunk_headfile)}];
+clear pda; % clear pda to save on memory. may not help much.
+[success_status,img, buffer]=rad_mat(scanner,runno,['rad_mat_chunk',patient_id,strjoin(strsplit(num2str(scan_ids),' '),'_')],...
+    [ {'existing_data','overwrite'},...
     testing_options,...
     rad_mat_options,...
     rad_mat_options_full_only,...
