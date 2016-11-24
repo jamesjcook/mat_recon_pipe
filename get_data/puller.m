edcf9dc12c230c61739125d60103eba543b84729
James Cook <jamesjcook@users.noreply.github.com>
2016-11-23 22:05:04 -0500

    Addded the repeat flag to puller usage so we can move that odd behavior out of matlab

--- get_data/puller.m
+++ get_data/puller.m
@@ -3,23 +3,36 @@ function puller(data_buffer,opt_struct,scanner,puller_data,output)
 % pull the data to local machine
 
 if ~exist('output','var')
-    if ~isfield(data_buffer.headfile,'work_dir_path')&& isfield(data_buffer.headfile,'U_runno')
+    if ~isfield(data_buffer.headfile,'work_dir_path') ...
+            && isfield(data_buffer.headfile,'U_runno') ...
+            && isfield(data_buffer,'engine_constants') ...
+            && isfield(data_buffer.engine_constants,'engine_work_directory')
         work_dir_name= [data_buffer.headfile.U_runno '.work'];
         data_buffer.headfile.work_dir_path=[data_buffer.engine_constants.engine_work_directory '/' work_dir_name];
         output=data_buffer.headfile.work_dir_path;
+    else
+        output='';
     end
 else
    % data_buffer.headfile.work_dir_path=output;
 end
-
-if opt_struct.overwrite
+if ~isfield(opt_struct,'puller_option_string')
+    opt_struct.puller_option_string = '';
+end
+if isfield(opt_struct,'overwrite') && opt_struct.overwrite==1
     opt_struct.puller_option_string=[' -o ' opt_struct.puller_option_string];
+else 
+    opt_struct.overwrite=0;
 end
-if opt_struct.existing_data && exist(output,'dir') %||opt_struct.skip_recon
+if  isfield(opt_struct,'existing_data') && opt_struct.existing_data==1 %&& (exist(output,'dir') || isempty(output) )
     opt_struct.puller_option_string=[' -e ' opt_struct.puller_option_string];
+else
+    opt_struct.existing_data=0;
 end
 
-cmd_list=['puller_simple ' opt_struct.puller_option_string ' ' scanner ' ''' puller_data ''' ' output];
+% cmd_list=['puller_simple ' opt_struct.puller_option_string ' ' scanner ' ''' puller_data ''' ' output];
+cmd_list=sprintf('puller_simple -r %s %s %s %s',opt_struct.puller_option_string,scanner,puller_data,output);
+
 if ~isfield(data_buffer.headfile,'comment')
     data_buffer.headfile.comment={};
 end
@@ -28,11 +41,12 @@ data_buffer.headfile.comment{end+1}=['# ' cmd_list ];
 data_buffer.headfile.comment{end+1}=['# /\ pull cmd ' '/\'];
 if ~opt_struct.existing_data || ~exist(output,'dir')  %&&~opt_struct.skip_recon
     if ~exist(output,'dir') && opt_struct.existing_data
-        warning('You wanted existing data BUT IT WASNT THERE!\n\tContinuing by tring to fetch new.');
+        warning(sprintf('You wanted existing data BUT IT WASNT THERE Or I dont know how to look!\n\tContinuing by tring to fetch new.\nMaybe puller will understand you.'));
         pause(1);
     end
     p_status =system(cmd_list);
-    if p_status ~= 0 && ~opt_struct.ignore_errors
+    if p_status ~= 0 ...
+            && ( ~isfield(opt_struct,'ignore_errors') || ~opt_struct.ignore_errors )
         error('puller failed:%s',cmd_list);
     end
 end
